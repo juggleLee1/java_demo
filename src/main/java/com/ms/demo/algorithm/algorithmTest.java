69d2059a1729f7ab0c738006e48acc7ff30a4049
lee <295082237@qq.com>
2024-04-09 17:45:28 +0800

    11

--- src/main/java/com/ms/demo/algorithm/algorithmTest.java
+++ src/main/java/com/ms/demo/algorithm/algorithmTest.java
@@ -1,6 +1,7 @@
 package com.ms.demo.algorithm;
 
-import java.util.BitSet;
+import java.nio.file.LinkOption;
+import java.util.*;
 import java.util.function.Predicate;
 
 /**
@@ -41,4 +42,114 @@ public class algorithmTest {
 
         return nums.length - remove_num;
     }
+
+    /***
+     * 树的遍历之层序遍历
+     * @param root
+     * @return
+     */
+    public static List<List<Integer>> levelOrder(TreeNode root) {
+        List<List<Integer>> res_list = new ArrayList<>();
+
+        if (root == null) return res_list;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+
+        queue.offer(root);
+        TreeNode node;
+        List<Integer> temp_list;
+        int len, i;
+        while (!queue.isEmpty()) {
+            temp_list = new ArrayList<>();
+            for (i = 0, len = queue.size(); i < len; i++) {
+                node = queue.poll();
+                if (node.left != null) queue.offer(node.left);
+                if (node.right != null) queue.offer(node.right);
+                temp_list.add(node.val);
+            }
+            res_list.add(temp_list);
+        }
+
+        return res_list;
+    }
+
+    /***
+     * 树的遍历之先序遍历：非递归
+     * @param root
+     * @return
+     */
+    public List<Integer> preorderTraversal(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+        List<Integer> list = new ArrayList<>();
+
+        Deque<TreeNode> deque = new LinkedList<>();
+        TreeNode node = root;
+        while (!deque.isEmpty() || node != null) {
+            while (node != null) {
+                deque.offerFirst(node);
+                list.add(node.val);
+                node = node.left;
+            }
+
+            node = deque.pollFirst();
+            node = node.right;
+        }
+
+        return list;
+    }
+
+    /***
+     * 树的中序遍历：非递归
+     * @return
+     */
+    public List<Integer> inorderTraversal(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+
+        Deque<TreeNode> deque = new LinkedList<>();
+        List<Integer> list = new ArrayList<>();
+        TreeNode node = root;
+
+        while (!deque.isEmpty() || node != null) {
+            while (node != null) {
+                deque.offerFirst(node);
+                node = node.left;
+            }
+
+            node = deque.pollFirst();
+            list.add(node.val);
+            node = node.right;
+        }
+
+        return list;
+    }
+
+
+    /***
+     * 树的后续遍历：非递归
+     * @param root
+     * @return
+     */
+    public List<Integer> postorderTraversal(TreeNode root) {
+        if (root == null) return new ArrayList<>();
+
+        Deque<TreeNode> deque1 = new LinkedList<>();
+        Deque<TreeNode> deque2 = new LinkedList<>();
+
+        deque1.offerFirst(root);
+        TreeNode node;
+        while (!deque1.isEmpty()) {
+            node = deque1.pollFirst();
+            deque2.offerFirst(node);
+            if (node.left != null) deque1.offerFirst(node.left);
+            if (node.right != null) deque1.offerFirst(node.right);
+        }
+
+        List<Integer> list = new ArrayList<>();
+        while (!deque2.isEmpty()) {
+            list.add(deque2.pollFirst().val);
+        }
+
+        return list;
+    }
 }
+
--- src/main/java/com/ms/demo/system/ListTest.java
+++ src/main/java/com/ms/demo/system/ListTest.java
@@ -1,16 +1,38 @@
 package com.ms.demo.system;
 
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.ListIterator;
 
 public class ListTest {
     public static void main(String[] args) {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-        arrayList.add(1);
-        arrayList.add(2);
-        arrayList.add(3);
+        LinkedList<Integer> linkedList = new LinkedList<>();
+        for (int i = 0; i < 10000000; i++) {
+            linkedList.add(i);
+        }
+        long start1 = System.currentTimeMillis();
+        Iterator<Integer> iterator = linkedList.iterator();
+        while (iterator.hasNext()) {
+            iterator.next();
+        }
+        long end1 = System.currentTimeMillis();
 
-        arrayList.replaceAll(n -> n * n);
+        System.out.println("普通迭代器花费时间: " + (end1 - start1));
 
-        System.out.println(arrayList);
+        long start2 = System.currentTimeMillis();
+        for (Integer integer : linkedList) {
+
+        }
+        long end2 = System.currentTimeMillis();
+        System.out.println("普通迭代器花费时间: " + (end2 - start2));
+
+        long start3 = System.currentTimeMillis();
+        ListIterator<Integer> listIterator = linkedList.listIterator(0);
+        while (listIterator.hasNext()) {
+            listIterator.next();
+        }
+        long end3 = System.currentTimeMillis();
+        System.out.println("普通迭代器花费时间: " + (end3 - start3));
     }
 }
